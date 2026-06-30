deac37db5079b2532ac37eac67b1b2f22671ca1e
Viktor Shuntikov <150535505+ViktorovVik@users.noreply.github.com>
2026-06-30 20:31:22 +0700

    feat: add header adaptive and mobile menu (#7)

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -1,33 +1,60 @@
+import { useEffect, useState } from 'react';
 import { NavLink } from 'react-router';
+import cn from 'clsx';
 import styles from './Header.module.scss';
 import { NAV_ITEMS } from '@/widgets/Header/config/navItems';
 import { Logo, Socials } from '@/shared/ui';
-import cn from 'clsx';
+import { BurgerButton } from '@/widgets/Header/ui/BurgerButton/BurgerButton.tsx';
 
 export const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleBurgerMenu = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      document.documentElement.classList.add('is-lock');
+    } else {
+      document.documentElement.classList.remove('is-lock');
+    }
+
+    return () => {
+      document.documentElement.classList.remove('is-lock');
+    };
+  }, [isOpen]);
+
   return (
     <header className={styles.header}>
       <div className={`${styles.headerInner}  container`}>
         <Logo />
-        <div className={styles.contentWrapper}>
-          <nav>
-            <ul className={styles.navList}>
-              {NAV_ITEMS.map(({ path, label }) => (
-                <li key={path}>
-                  <NavLink
-                    to={path}
-                    className={({ isActive }) =>
-                      cn(styles.link, isActive && styles.active)
-                    }
-                  >
-                    {label}
-                  </NavLink>
-                </li>
-              ))}
-            </ul>
-          </nav>
-          <Socials />
+        <div className={cn(styles.overlay, isOpen && styles.open)}>
+          <div className={styles.contentWrapper}>
+            <nav>
+              <ul className={styles.navList}>
+                {NAV_ITEMS.map(({ path, label }) => (
+                  <li key={path}>
+                    <NavLink
+                      to={path}
+                      className={({ isActive }) =>
+                        cn(styles.link, isActive && styles.active)
+                      }
+                    >
+                      {label}
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+            <Socials />
+          </div>
         </div>
+        <BurgerButton
+          className="visible-mobile"
+          isOpen={isOpen}
+          toggleBurgerMenu={toggleBurgerMenu}
+        />
       </div>
     </header>
   );
