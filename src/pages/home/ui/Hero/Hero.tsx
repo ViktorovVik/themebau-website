3b1c3f81e9ddfb1802e6dd425049e106e4c32954
Viktor Shuntikov <150535505+ViktorovVik@users.noreply.github.com>
2026-07-20 16:41:07 +0700

    feat/hero section (#9)
    * feat: add hero section
    
    * fix: remove header border
    
    * feat: add hero background
    
    * feat: add hero responsive layout

---
+++ src/pages/home/ui/Hero/Hero.tsx
@@ -0,0 +1,51 @@
+import { Link } from 'react-router';
+import slide1 from '@/shared/assets/img/slider/3.webp';
+import slide2 from '@/shared/assets/img/slider/2.webp';
+import slide3 from '@/shared/assets/img/slider/1.webp';
+import { Slider } from '@/shared/ui';
+import style from './Hero.module.scss';
+import cn from 'clsx';
+
+export const Hero = () => {
+  const slides = [
+    <img
+      key="1"
+      src={slide1}
+      alt=""
+      width={1170}
+      height={580}
+      loading="eager"
+    />,
+    <img
+      key="2"
+      src={slide2}
+      alt=""
+      width={1170}
+      height={580}
+      loading="eager"
+    />,
+    <img
+      key="3"
+      src={slide3}
+      alt=""
+      width={1170}
+      height={580}
+      loading="eager"
+    />,
+  ];
+
+  return (
+    <section className={style.hero}>
+      <div className={cn(style.inner, 'container')}>
+        <Slider slides={slides} navigation className={style.heroSlider} />
+        <div className={style.content}>
+          <span className={style.line}></span>
+          <h1 className={cn(style.title, 'h1')}>Colibri Creative Project</h1>
+        </div>
+        <Link to="/portfolio" className={style.view}>
+          view
+        </Link>
+      </div>
+    </section>
+  );
+};
--- src/shared/ui/Slider/Slider.tsx
+++ src/shared/ui/Slider/Slider.tsx
@@ -1,29 +1,68 @@
-import type { ReactNode } from 'react';
+import { type ReactNode } from 'react';
 import { Navigation, Pagination, A11y } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-// import styles from './Slider.module.scss';
+import styles from './Slider.module.scss';
+import cn from 'clsx';
 
 interface SliderProps {
   className?: string;
   slides: ReactNode[];
+  navigation?: boolean;
 }
 
-export const Slider = ({ slides, className }: SliderProps) => {
+export const Slider = ({
+  slides,
+  className,
+  navigation = true,
+}: SliderProps) => {
   return (
-    <Swiper
-      className={className}
-      modules={[Navigation, Pagination, A11y]}
-      slidesPerView={1}
-      navigation
-      loop={true}
-      pagination={{ clickable: false }}
-    >
-      {slides.map((slide, index) => (
-        <SwiperSlide key={index}>{slide}</SwiperSlide>
-      ))}
-    </Swiper>
+    <div className={styles.sliderWrapper}>
+      <Swiper
+        className={cn(styles.swiper, className)}
+        modules={[Navigation, Pagination, A11y]}
+        slidesPerView={1}
+        navigation={{
+          prevEl: '.swiper-arrow-prev',
+          nextEl: '.swiper-arrow-next',
+        }}
+        loop={true}
+        pagination={{
+          type: 'fraction',
+          renderFraction: (currentClass, totalClass) =>
+            `<span class="${currentClass}"></span> <span class="divider"> — </span> <span class="${totalClass}"></span>`,
+          formatFractionCurrent: (number) =>
+            number < 10 ? '0' + number : number,
+          formatFractionTotal: (number) =>
+            number < 10 ? '0' + number : number,
+        }}
+      >
+        {slides.map((slide, index) => (
+          <SwiperSlide key={index}>{slide}</SwiperSlide>
+        ))}
+      </Swiper>
+      {navigation && (
+        <div className="hidden-tablet">
+          <button
+            className={cn(styles.prev, 'swiper-arrow-prev')}
+            type="button"
+          >
+            <svg className={styles.svgEl}>
+              <use href="/sprites/sprites.svg#arrow" />
+            </svg>
+          </button>
+          <button
+            className={cn(styles.next, 'swiper-arrow-next')}
+            type="button"
+          >
+            <svg className={styles.svgEl}>
+              <use href="/sprites/sprites.svg#arrow" />
+            </svg>
+          </button>
+        </div>
+      )}
+    </div>
   );
 };
