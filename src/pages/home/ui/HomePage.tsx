40ec0bd93f1b5b96eedab52a67a508ace233cbcc
schuntikov.viktor@yandex.ru <schuntikov.viktor@yandex.ru>
2026-07-21 20:27:25 +0700

    feat: add about section

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -1,9 +1,11 @@
 import { Hero } from './Hero';
+import { About } from './About';
 
 export const HomePage = () => {
   return (
     <>
       <Hero />
+      <About />
     </>
   );
 };
---
+++ src/shared/ui/Subtitle/Subtitle.tsx
@@ -0,0 +1,12 @@
+import type { ReactNode } from "react";
+import cn from "clsx";
+import style from './Subtitle.module.scss';
+
+interface SubtitleProps {
+  className?: string;
+  children: ReactNode;
+}
+
+export const Subtitle = ({ className, children }: SubtitleProps) => {
+  return <span className={cn(style.subtitle, className)}>{children}</span>
+}
