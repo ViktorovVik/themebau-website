b0da2fcb87a81187ad573945eb9f871d5811b8c4
Viktor Shuntikov <150535505+ViktorovVik@users.noreply.github.com>
2026-07-23 01:44:26 +0700

    feat: add services section (#17)

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -1,11 +1,13 @@
 import { Hero } from './Hero';
 import { About } from './About';
+import { Services } from "./Services";
 
 export const HomePage = () => {
   return (
     <>
       <Hero />
       <About />
+      <Services />
     </>
   );
 };
---
+++ src/pages/home/ui/Services/ui/ServicesCard/ServiceCard.tsx
@@ -0,0 +1,34 @@
+import { type ServicesCard } from "../../config/services.data";
+import styles from './ServiceCard.module.scss';
+import { Button } from "@/shared/ui";
+
+
+type Props = Omit<ServicesCard, 'id'>;
+
+
+export const ServiceCard = ({src, iconId, title, to}: Props) => {
+  return (
+    <article className={styles.card}>
+      <img
+        className={styles.cardImg}
+        src={src}
+        alt=""
+        width={304}
+        height={400}
+        loading="lazy"
+      />
+      <div className={styles.cardContent}>
+        <svg aria-hidden="true" className={styles.iconSvg}>
+          <use href={`/sprites/sprites.svg#${iconId}`}></use>
+        </svg>
+        <h3 className={styles.cardTitle}>{title}</h3>
+        <Button className={styles.cardLink} variant='transparent' to={to}>
+          read more
+          <svg aria-hidden="true" className={styles.arrow}>
+            <use href="/sprites/sprites.svg#arrow" />
+          </svg>
+        </Button>
+      </div>
+    </article>
+  )
+}
