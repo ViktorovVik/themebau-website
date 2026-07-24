1ce6249071d020d8f9d80743181a2d600e05b624
schuntikov.viktor@yandex.ru <schuntikov.viktor@yandex.ru>
2026-07-24 19:52:16 +0700

    feat: add work process section

---
+++ src/pages/home/ui/WorkProcess/config/workprocess.data.ts
@@ -0,0 +1,28 @@
+export interface WorkProcessData {
+  id: string;
+  title: string;
+  text: string;
+}
+
+export const WORK_PROCESS: WorkProcessData[] = [
+  {
+    id: '1',
+    title: 'Search an Idea',
+    text: "God he their every blessed upon  male earth also midst fill creeping Seas their winged can't shall signs sea mornings gathered brought, creeping fruitful you'll open there bearing.",
+  },
+  {
+    id: '2',
+    title: 'Design',
+    text: "Signs sea mornings gathered brought, creeping fruitful you'll open there bearing.",
+  },
+  {
+    id: '3',
+    title: 'Development',
+    text: "God he their every blessed upon male earth also midst fill creeping Seas their winged can't shall",
+  },
+  {
+    id: '4',
+    title: 'Release',
+    text: "Midst fill creeping Seas their winged can't shall signs sea mornings gathered brought, creeping",
+  },
+];
