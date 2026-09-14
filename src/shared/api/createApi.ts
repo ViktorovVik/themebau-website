3686c479284bc6030f12ee3dcb882e14f10dfb94
schuntikov.viktor@yandex.ru <schuntikov.viktor@yandex.ru>
2026-09-14 13:56:19 +0700

    refactor: replace json-server with supabase

--- src/shared/api/createApi.ts
+++ src/shared/api/createApi.ts
@@ -1,24 +1,68 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi } from '@reduxjs/toolkit/query/react';
 import type { ProjectsData, ContactFormData, BlogData } from './types';
+import { supabase } from './supabase';
+
+type TableDataSelect = {
+  table: string;
+  action: 'select';
+  fields: string;
+};
+
+type TableDataInsert = {
+  table: string;
+  action: 'insert';
+  data: ContactFormData;
+};
+
+type TableDataTypes = TableDataSelect | TableDataInsert;
+
+const customBaseQuery = async (base: TableDataTypes) => {
+  const { table, action } = base;
+
+  let insertOrSelectData;
+
+  if (action === 'insert') {
+    insertOrSelectData = await supabase.from(table).insert(base.data);
+  } else {
+    insertOrSelectData = await supabase.from(table).select(base.fields);
+  }
+
+  const { data, error } = insertOrSelectData;
+
+  if (error) {
+    return { error };
+  } else {
+    return { data };
+  }
+};
 
 export const projectsApi = createApi({
   reducerPath: 'projectsApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3001/' }),
+  baseQuery: customBaseQuery,
   endpoints: (builder) => ({
     getProjects: builder.query<ProjectsData[], void>({
-      query: () => `projects/`,
+      query: () => ({
+        table: 'projects',
+        action: 'select',
+        fields: 'id, imgSrc:img_src, category, to:link, title',
+      }),
     }),
     postMessage: builder.mutation<{ id: string }, ContactFormData>({
       query: (formData) => ({
-        url: 'messages/',
-        method: 'POST',
-        body: formData,
+        table: 'messages',
+        action: 'insert',
+        data: formData,
       }),
     }),
     getPosts: builder.query<BlogData[], void>({
-      query: () => `posts/`,
-    })
+      query: () => ({
+        table: 'posts',
+        action: 'select',
+        fields: 'id, date, imgBg:img_bg, to:link, title',
+      }),
+    }),
   }),
 });
 
-export const { useGetProjectsQuery, usePostMessageMutation, useGetPostsQuery } = projectsApi;
+export const { useGetProjectsQuery, usePostMessageMutation, useGetPostsQuery } =
+  projectsApi;
